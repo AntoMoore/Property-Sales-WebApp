b067055c70fd2b01fa668c940ceab37867b95124
anto <amooretmp@gmail.com>
2022-01-10 14:38:54 +0000

    added navigation bar

--- home/home.go
+++ home/home.go
@@ -1,27 +1,47 @@
 package main
 
 import (
-	"bytes"
+	//"bytes"
 	"html/template"
 	"net/http"
-	"net/url"
-	"time"
+	//"net/url"
+	//"time"
 
 	"example.com/resources"
 )
 
 // reference to index.html
-var tpl = template.Must(template.ParseFiles("../template/index.html"))
+var templates = template.Must(template.ParseFiles("../templates/header.html", "../templates/footer.html", "../templates/header.html", "../templates/main.html", "../templates/about.html"))
+
+type Page struct {
+	Title string
+}
 
 type AgentSearch struct {
 	Query      		string
 	AgentResults    *resources.AgentResults
 }
 
+//Display the named template
+func display(w http.ResponseWriter, tmpl string, data interface{}) {
+	templates.ExecuteTemplate(w, tmpl, data)
+}
+
+//The handlers.
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "main", &Page{Title: "Home"})
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "about", &Page{Title: "About"})
+}
+
+
 // template execution handler
+/*
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	buf := &bytes.Buffer{}
-	err := tpl.Execute(buf, nil)
+	err := templates.Execute(buf, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -29,8 +49,10 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	buf.WriteTo(w)
 }
+*/
 
 // handle search paramaters
+/*
 func searchHandler(resourceApi *resources.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// check URL for errors
@@ -59,7 +81,7 @@ func searchHandler(resourceApi *resources.Client) http.HandlerFunc {
 
 		// parse AgentSearch
 		buf := &bytes.Buffer{}
-		err = tpl.Execute(buf, agentSearch)
+		err = templates.Execute(buf, agentSearch)
 
 		// check for IO errors
 		if err != nil {
@@ -70,22 +92,32 @@ func searchHandler(resourceApi *resources.Client) http.HandlerFunc {
 		buf.WriteTo(w)
 	}
 }
+*/
 
 func main() {
 	// Server PORT
 	port := "3000"
 
 	// Resource Client
-	myClient := &http.Client{Timeout: 10 * time.Second}
-	resourceApi := resources.NewClient(myClient)
+	//myClient := &http.Client{Timeout: 10 * time.Second}
+	//resourceApi := resources.NewClient(myClient)
 
 	// static styling sheets
 	fs := http.FileServer(http.Dir("../assets"))
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
+	//mux.HandleFunc("/", indexHandler)
+	//mux.HandleFunc("/search", searchHandler(resourceApi))
+	//http.ListenAndServe(":"+port, mux)
+
 	// URL routing
-	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/search", searchHandler(resourceApi))
+	mux.HandleFunc("/", mainHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	//Listen on port 3000
 	http.ListenAndServe(":"+port, mux)
+
+
+	
 }
